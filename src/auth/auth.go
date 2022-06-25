5d2ece7368e1d8914cfb15cc80cf612c9f2ee2a8
Matheus Hermes <77905781+matheushermes@users.noreply.github.com>
2022-06-24 21:34:48 -0300

    create package auth
    returns a signed token

--- src/auth/auth.go
+++ src/auth/auth.go
@@ -1,1 +1,18 @@
 package auth
+
+import (
+	"time"
+
+	jwt "github.com/dgrijalva/jwt-go"
+)
+
+//CreateToken retorna um token assinado com as permissões do usuário;
+func CreateToken(userId uint64) (string, error) {
+	permissions := jwt.MapClaims{}
+	permissions["authorized"] = true
+	permissions["exp"] = time.Now().Add(time.Hour * 8).Unix()
+	permissions["userId"] = userId
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, permissions)
+
+	return token.SignedString([]byte("Secret"))
+}
