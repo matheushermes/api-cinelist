ba1b2308b58250fa85ef50bc16aeac2eb1d0a2ab
Matheus Hermes <77905781+matheushermes@users.noreply.github.com>
2022-06-26 20:05:20 -0300

    create controllers animes

---
+++ src/controllers/animes.go
@@ -0,0 +1,33 @@
+package controllers
+
+import "net/http"
+
+//CreateNewAnime vai adicionar um novo anime a lista de animes já assistidos;
+func CreateNewAnime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Adicionando um novo anime"))
+}
+
+//SearchAnimeList traz todos os animes adicionados pelo usuário em sua lista de animes já assistidos;
+func SearchAnimeList(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando sua lista de animes assistidos"))
+}
+
+//SearchAnime traz o anime escolhido pelo usuário;
+func SearchAnime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Buscando anime mencionado"))
+}
+
+//UpdateAnime altera os dados de um anime adicionado;
+func UpdateAnime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Atualizando anime"))
+}
+
+//DeleteAnime altera os dados de um anime adicionado;
+func DeleteAnime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Deletando anime"))
+}
+
+//AddFavoriteAnime adiciona um anime a sua lista de animes favoritos;
+func AddFavoriteAnime(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Adicionando anime a lista de favoritos"))
+}
---
+++ src/router/routers/animes.go
@@ -0,0 +1,45 @@
+package routers
+
+import (
+	"cinelist/src/controllers"
+	"net/http"
+)
+
+var routerAnimeList = []Router{
+	{
+		URI:    		"/anime",
+		Method: 		http.MethodPost,
+		Func: 			controllers.CreateNewAnime,
+		RequiresAuth: 	true,
+	},
+	{
+		URI:    		"/animes",
+		Method: 		http.MethodGet,
+		Func: 			controllers.SearchAnimeList,
+		RequiresAuth: 	true,
+	},
+	{
+		URI:    		"/anime/{animeId}",
+		Method: 		http.MethodGet,
+		Func: 			controllers.SearchAnime,
+		RequiresAuth: 	true,
+	},
+	{
+		URI:    		"/anime/{animeId}",
+		Method: 		http.MethodPut,
+		Func: 			controllers.UpdateAnime,
+		RequiresAuth: 	true,
+	},
+	{
+		URI:    		"/anime/{animeId}",
+		Method: 		http.MethodDelete,
+		Func: 			controllers.CreateNewAnime,
+		RequiresAuth: 	true,
+	},
+	{
+		URI:    		"/anime",
+		Method: 		http.MethodPost,
+		Func: 			controllers.CreateNewAnime,
+		RequiresAuth: 	true,
+	},
+}
