15cf207a618f066ea17a287cd5a20be677884bf1
Matheus Hermes <77905781+matheushermes@users.noreply.github.com>
2022-07-14 20:59:36 -0300

    solving problem with cors

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -18,7 +18,10 @@ import (
 
 //CreateUser vai registrar um usuário no banco de dados;
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token")
+	w.Header().Set("Access-Control-Allow-Credentials", "true")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 
 	//Lendo o corpo da requisição;
 	bodyRequest, err := ioutil.ReadAll(r.Body);
