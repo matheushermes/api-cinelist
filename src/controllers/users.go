591f553ed77840d42841adb4d4bf633156c32ced
Matheus Hermes <77905781+matheushermes@users.noreply.github.com>
2022-07-18 19:33:44 -0300

    solving cors problem

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -22,6 +22,10 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
 
+	if (r.Method == "OPTIONS") {
+		w.Header().Set("Access-Control-Allow-Headers", "Access-Control-Allow-Headers")
+	}
+
 	//Lendo o corpo da requisição;
 	bodyRequest, err := ioutil.ReadAll(r.Body);
 	if err != nil {
